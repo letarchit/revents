ef6747926a0e40d37b74020c31ffdb4375893914
letarchit <archit937@gmail.com>
2020-10-30 04:07:28 +0530

    Section 2 updates

---
+++ src/features/event/EventList/dcm.c
@@ -0,0 +1,18 @@
+# include<stdio.h>
+# include<conio.h>
+
+main();
+{   clrscr(0) 
+int a;
+int b;
+
+printf("enter the first number:");
+scanf("%d,&a")
+
+printf("Enter the second no ")
+scanf("%d,&b")
+
+int c = a + b;
+printf("The sum of two no is: %d,&c" )
+
+getch;
